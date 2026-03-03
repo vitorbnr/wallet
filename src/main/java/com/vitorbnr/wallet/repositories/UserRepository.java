98085adfaa6b3ee0ad99d15574d0ac790242ce76
vitorbnr <vitorbernardotech@gmail.com>
2026-03-02 21:22:44 -0300

    fix: import

--- src/main/java/com/vitorbnr/wallet/repositories/UserRepository.java
+++ src/main/java/com/vitorbnr/wallet/repositories/UserRepository.java
@@ -1,12 +1,13 @@
 package com.vitorbnr.wallet.repositories;
 
 import com.vitorbnr.wallet.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 import java.util.UUID;
 
-public interface UserRepository extends CrudRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findUserByDocument(String document);
 
